5062585765cf80257c41cbeef1b54f0db44f4c5a
Lydia Kwa <lydiakwa@gmail.com>
2022-10-27 14:45:32 -0400

    FEAT: created new post form added back home links to all pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  #shows the form -- new.html.erb
+  def new
+    @article = Article.new
+  end
+
+  #this does not have a view
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      #render :new -- renders the erb file BUT does not call new method
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 
 end
